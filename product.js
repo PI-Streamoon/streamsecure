4ebffcd1996c8881fe31fa3be8f8aa07d5e78d82
Lucas Ferreira Bezerra <125586165+Lucas1Ferreira@users.noreply.github.com>
2023-08-17 16:07:55 -0300

    Add files via upload
    modal

---
+++ product.js
@@ -0,0 +1,12 @@
+
+
+    function openModal() {
+        document.getElementById("fade").style.display = "block";
+        document.getElementById("modal").style.display = "block";
+      }
+      function closeModal() {
+        document.getElementById("fade").style.display = "none";
+        document.getElementById("modal").style.display = "none";
+      }
+  
+     
