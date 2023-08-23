90bde8a445a0b2e382e375da96022c096f5fe3cf
eduufreire <eduardo.freire@sptech.school>
2023-08-23 20:59:09 -0300

    Finalizando a responsividade - Mobile.v2

--- site/public/js/product.js
+++ site/public/js/product.js
@@ -1,18 +1,4 @@
 
-  window.onresize = ()=>{
-    var largura = window.innerWidth
-
-    console.log('aaa')
-    console.log(largura)
-
-    if(largura < 800){
-      document.getElementById('logo-navbar').setAttribute('src', './features/img/logo.png')
-    }else if(largura > 801){
-      document.getElementById('logo-navbar').setAttribute('src', './features/img/nome-e-logo 1.png')
-    }
-  }
-
-
     function openModal() {
         document.getElementById("fade").style.display = "flex";
         document.getElementById("modal").style.display = "flex";
