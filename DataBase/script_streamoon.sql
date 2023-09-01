3b510d1b2348f4c2ebd71604e16736caf8b00086
eduufreire <eduardo.freire@sptech.school>
2023-08-31 21:46:02 -0300

    Atualizando branch para fazer merge na QA

--- DataBase/script_streamoon.sql
+++ DataBase/script_streamoon.sql
@@ -1,17 +1,18 @@
 CREATE database streamoon;
 USE streamoon;
 drop database streamoon;
-
 CREATE TABLE IF NOT EXISTS empresa(
-  idEmpresa INT NOT NULL,
+  idEmpresa INT NOT NULL auto_increment,
   nome VARCHAR(45) NULL,
   cnpj CHAR(14) NULL,
   localidade VARCHAR(45) NULL,
-  PRIMARY KEY (idEmpresa));
+  PRIMARY KEY (idEmpresa))
+  auto_increment = 484018;
   
+  select * from empresa;
 
 CREATE TABLE IF NOT EXISTS usuario (
-  idUsuario INT NOT NULL,
+  idUsuario INT NOT NULL auto_increment,
   fkEmpresa INT NOT NULL,
   fkAdmin INT,
   nome varchar(50) not null,
@@ -29,23 +30,27 @@ CREATE TABLE IF NOT EXISTS usuario (
     REFERENCES usuario (`idUsuario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
+    auto_increment = 1
 ;
+  select * from usuario;
 
 CREATE TABLE IF NOT EXISTS locais (
-  idLocais INT NOT NULL,
+  idLocais INT NOT NULL auto_increment,
   fkEmpresa INT NOT NULL,
   cep VARCHAR(45) NULL,
+  descricao varchar(100) null,
   PRIMARY KEY (`idLocais`),
   CONSTRAINT `fk_Locais_Empresa1`
     FOREIGN KEY (`fkEmpresa`)
     REFERENCES empresa (`idEmpresa`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
+    auto_increment = 100
 ;
+select * from locais; 
 
 CREATE TABLE IF NOT EXISTS servidor (
-  idServidor INT NOT NULL,
-  nomeServidor VARCHAR(80) NOT NULL,
+  idServidor INT NOT NULL auto_increment,
   fkLocais INT NOT NULL,
   PRIMARY KEY (idServidor),
   CONSTRAINT `fk_Servidor_Locais1`
@@ -53,16 +58,21 @@ CREATE TABLE IF NOT EXISTS servidor (
     REFERENCES locais (`idLocais`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
+    auto_increment = 2222
     ;
+    select * from servidor
+    join locais on fkLocais = idLocais;
 
 CREATE TABLE IF NOT EXISTS unidadeMedida (
-  idUnidadeMedida INT NOT NULL,
+  idUnidadeMedida INT NOT NULL auto_increment,
   nomeMedida varchar(35) not null,
   PRIMARY KEY (`idUnidadeMedida`))
+  auto_increment = 1
 ;
+select * from unidadeMedida;
 
 CREATE TABLE IF NOT EXISTS componente (
-  idComponente INT NOT NULL,
+  idComponente INT NOT NULL auto_increment,
   fkUnidadeMedida INT NOT NULL,
   nome varchar(50) not null,
   PRIMARY KEY (`idComponente`, `fkUnidadeMedida`),
@@ -70,41 +80,45 @@ CREATE TABLE IF NOT EXISTS componente (
     FOREIGN KEY (`fkUnidadeMedida`)
     REFERENCES unidadeMedida (`idUnidadeMedida`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-);
+    ON UPDATE NO ACTION)
+    auto_increment = 100;
+    
+-- select * from componente;
+-- SET SQL_SAFE_UPDATES = 0;
+-- update componente set nome = 'Memoria' where nome = 'Memória';
+-- update componente set nome = 'MemoriaTotal' where nome = 'Memória Total';
+-- update componente set nome = 'MemoriaUsada' where nome = 'Memória Usada';
 
 CREATE TABLE IF NOT EXISTS componenteServidor (
-  `idComponenteServidor` INT NOT NULL,
+  `idComponenteServidor` INT NOT NULL auto_increment,
   `fkServidor` INT NOT NULL,
   `fkComponente` INT NOT NULL,
-  `fkUnidadeMedida` INT NOT NULL,
-  PRIMARY KEY (`idComponenteServidor`, `fkServidor`, `fkComponente`, `fkUnidadeMedida`),
+  PRIMARY KEY (`idComponenteServidor`, `fkServidor`, `fkComponente`),
   CONSTRAINT `fk_Componente_has_Servidor_Servidor1`
     FOREIGN KEY (`fkServidor`)
     REFERENCES servidor (`idServidor`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_ComponenteServidor_Componente1`
-    FOREIGN KEY (`fkComponente` , `fkUnidadeMedida`)
-    REFERENCES componente (`idComponente` , `fkUnidadeMedida`)
+    FOREIGN KEY (`fkComponente`)
+    REFERENCES componente (`idComponente`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
+    auto_increment = 1
 ;
 
 CREATE TABLE IF NOT EXISTS registro (
-  `idRegistro` INT NOT NULL,
+  `idRegistro` INT NOT NULL auto_increment,
   `registro` INT NULL,
   `dtHora` DATETIME NULL,
   `fkComponenteServidor` INT NOT NULL,
-  `fkServidor` INT NOT NULL,
-  `fkComponente` INT NOT NULL,
-  `fkUnidadeMedida` INT NOT NULL,
-  PRIMARY KEY (`idRegistro`, `fkComponenteServidor`, `fkServidor`, `fkComponente`, `fkUnidadeMedida`),
+  PRIMARY KEY (`idRegistro`, `fkComponenteServidor`),
   CONSTRAINT `fk_Registro_ComponenteServidor1`
-    FOREIGN KEY (`fkComponenteServidor` , `fkServidor` , `fkComponente` , `fkUnidadeMedida`)
-    REFERENCES componenteServidor (`idComponenteServidor` , `fkServidor` , `fkComponente` , `fkUnidadeMedida`)
+    FOREIGN KEY (`fkComponenteServidor`)
+    REFERENCES componenteServidor (`idComponenteServidor`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
+    auto_increment = 100000;
 ;
 
 -- Inserção de dados
@@ -112,56 +126,165 @@ CREATE TABLE IF NOT EXISTS registro (
 -- Tabela empresa
 INSERT INTO empresa (idEmpresa, nome, cnpj, localidade)
 VALUES
-  (1, 'Empresa A', '12345678901234', 'Cidade A'),
-  (2, 'Empresa B', '98765432101234', 'Cidade B');
-
+  (null, 'HBOMax', '12345678901234', 'Centro de São Paulo'),
+  (null, 'Netflix', '98765432101234', 'São Jorge da Serra - Perdizes');
+  
+-- select * from empresa;
 -- Tabela usuario
 INSERT INTO usuario (idUsuario, fkEmpresa, fkAdmin, nome, senha, cpf, email)
 VALUES
-  (1, 1, NULL, 'Usuário A', 'senha123', '12345678901', 'usuarioa@example.com'),
-  (2, 2, 1, 'Usuário B', 'senha456', '98765432109', 'usuariob@example.com'),
-  (3, 1, 2, 'Usuário C', 'senha789', '45678912301', 'usuarioc@example.com');
+  (null, 484018, NULL, 'Fernando Brandão', '203457', '12345678901', 'brandao@gmail.com'),
+  (null, 484019, 1, 'Marise', 'senha456293', '12345678902','marise@gmail.com');
 
 -- Tabela locais
-INSERT INTO locais (idLocais, fkEmpresa, cep)
+INSERT INTO locais (idLocais, fkEmpresa, cep, descricao)
 VALUES
-  (1, 1, '12345-678'),
-  (2, 2, '98765-432');
+  (null, 484018, '12345-678', 'Local X, Andar 2'),
+  (null, 484018, '98765-432', 'Local Y, Andar 12');
+-- select * from locais;
 
 -- Tabela servidor
-INSERT INTO servidor (idServidor, nomeServidor, fkLocais)
+INSERT INTO servidor (idServidor, fkLocais)
 VALUES
-  (1, 'Teste 1',1),
-  (2, 'Teste 2', 2);
+  (null, 100),
+  (null, 101);
+-- select * from servidor;
 
 -- Tabela unidadeMedida
 INSERT INTO unidadeMedida (idUnidadeMedida, nomeMedida)
 VALUES
-  (1, 'Unidade'),
-  (2, 'Litro');
-
+  (null, 'GHZ'),
+  (null, 'GB'),
+  (null, 'KBPS'),
+  (null, '%');
+-- select * from unidadeMedida;
+ 
 -- Tabela componente
 INSERT INTO componente (idComponente, fkUnidadeMedida, nome)
 VALUES
-  (7, 1, 'Componente A'),
-  (10, 2, 'Componente B');
+  (null, 4, 'CPU'),
+  (null, 4, 'Memoria'),
+  (null, 2, 'MemoriaUsada'),
+  (null, 2, 'MemoriaTotal'),
+  (null, 4, 'Disco');
+ select * from componente;
 
 -- Tabela componenteServidor
-INSERT INTO componenteServidor (idComponenteServidor, fkServidor, fkComponente, fkUnidadeMedida)
+INSERT INTO componenteServidor (idComponenteServidor, fkServidor, fkComponente)
 VALUES
-  (100, 1, 7, 1),
-  (101, 2, 10, 2);
+  (null, 2222, 100),
+  (null, 2222, 101),
+  (null, 2222, 102),
+  (null, 2222, 103),
+  (null, 2222, 104);
+-- select * from componenteServidor;
 
 -- Tabela registro
-INSERT INTO registro (idRegistro, registro, dtHora, fkComponenteServidor, fkServidor, fkComponente, fkUnidadeMedida)
+INSERT INTO registro (idRegistro, registro, dtHora, fkComponenteServidor)
 VALUES
-  (1000, 20348034, '2023-08-01 10:00:00', 100, 1, 7, 1),
-  (1001, 02475092, '2023-08-02 15:30:00', 101, 2, 10, 2);
+  (null, 20348034, '2023-08-01 10:00:00', 1),
+  (null, 02475092, '2023-08-02 15:30:00', 2);
+select * from registro;
+
+  
+  select * from componenteServidor;
+  
+  SELECT r.registro, r.dtHora, c.nome, um.nomeMedida
+FROM registro r
+JOIN componenteServidor cs ON r.fkComponenteServidor = cs.idComponenteServidor
+JOIN componente c ON cs.fkComponente = c.idComponente
+JOIN unidadeMedida um ON c.fkUnidadeMedida = um.idUnidadeMedida;
+
+  
+  -- SELECT PARA SELEÇÃO DE TODOS OS REGISTROS DOS COMPONENTES COM SUA UNIDADE DE MEDIDA DE CADA SERVIDOR DE CADA LOCAL DE CADA EMPRESA
+select empresa.nome, locais.idLocais, servidor.idServidor, 
+componenteServidor.idComponenteServidor, 
+componente.idComponente, unidadeMedida.nomeMedida,
+registro.registro, registro.dtHora from registro
+join componenteServidor on idComponenteServidor = fkComponenteServidor
+join componente on idComponente = fkComponente
+join unidadeMedida on idUnidadeMedida = fkUnidadeMedida
+join servidor on idServidor = fkServidor
+join locais on idLocais = fkLocais
+join empresa on idEmpresa = fkEmpresa;
+
+SELECT r.registro, r.dtHora, c.nome AS nomeComponente, um.nomeMedida
+FROM registro r
+LEFT JOIN componenteServidor cs ON r.fkComponenteServidor = cs.idComponenteServidor
+LEFT JOIN componente c ON cs.fkComponente = c.idComponente
+LEFT JOIN unidadeMedida um ON c.fkUnidadeMedida = um.idUnidadeMedida
+ORDER BY um.nomeMedida, c.nome;
 
 
-select servidor.idServidor, registro, componente.nome from registro
-join componenteServidor on fkComponenteServidor = idComponenteServidor
-join servidor on idServidor = registro.fkServidor
-join componente on idComponente = registro.fkComponente
-join unidadeMedida on idUnidadeMedida = registro.fkUnidadeMedida;
 
+-- CÓDIGO DA CRIAÇÃO DA VIEW PARA VISUALIZAÇÃO DOS DADOS EM TABELA --------------------------------------------------------------------------------------------
+
+  create view tabelaRegistros as 
+  select registro.registro as 'Registro', registro.dtHora as 'MomentoRegistro', 
+  componente.nome as 'Componente', unidadeMedida.nomeMedida as 'Símbolo', 
+  componenteServidor.idComponenteServidor, servidor.idServidor as 'idServidor' from registro 
+  join componenteServidor on fkComponenteServidor = idComponenteServidor
+  join servidor on fkServidor = idServidor
+  join componente on fkComponente = idComponente
+  join unidadeMedida on fkUnidadeMedida = idUnidadeMedida 
+  -- where idComponenteServidor = 2
+  order by 2 and 3 and 4
+  limit 10000;
+  
+  select * from tabelaRegistros;
+
+  select MomentoRegistro, 
+  max(case when Componente = 'CPU' then Registro end) 'CPU',
+  max(case when Componente = 'Memoria' then Registro end) 'Memória',
+  max(case when Componente = 'MemoriaTotal' then Registro end) 'Memória Total',
+  max(case when Componente = 'MemoriaUsada' then Registro end) 'Memória Usada'
+  from tabelaRegistros
+  group by MomentoRegistro;
+  
+    select MomentoRegistro, Registro, Componente
+  from tabelaRegistros
+  group by MomentoRegistro, Registro, Componente
+  order by MomentoRegistro;
+  
+  select * from tabelaRegistros;
+
+SET @sql = NULL; -- Criando uma variável para armazenar o comando
+
+SELECT
+  GROUP_CONCAT(DISTINCT
+    CONCAT(
+      'max(case when Componente = ''',
+      Componente, -- aqui vem o nome que você setou para os componentes na view!
+      ''' then Registro end) ',
+      Componente -- aqui vem o nome que você setou para os componentes na view!
+    )
+  )
+INTO @sql
+
+FROM
+  tabelaRegistros; -- Aqui vem o nome da sua view!
+  
+-- max(case when Componente = 'Componente1' then Registro end) Componente1,
+-- max(case when Componente = 'Componente2' then Registro end) Componente2, .....
+select @sql;
+
+
+SET @sql = CONCAT('SELECT idServidor, MomentoRegistro, ', @sql, '
+                 
+FROM tabelaRegistros
+                   
+GROUP BY idServidor, MomentoRegistro'); -- Lembra de trocar as informações (idServidor, MomentoRegistro, tabelaRegistros) pelos nomes que você usou na view
+
+select @sql;
+
+PREPARE stmt FROM @sql; -- Prepara um statement para executar o comando guardado na variável @sql
+
+EXECUTE stmt; -- Executa o statement
+
+DEALLOCATE PREPARE stmt;
+
+
+
+
+
+-- FIM DO CÓDIGO ------------------------------------------------------------------------------------------------------------------------------------------- 
